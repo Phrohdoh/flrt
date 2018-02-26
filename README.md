0559155d5fb8d73f179b9f7f26facaaa2686ba7a
Taryn Hill <taryn@phrohdoh.com>
2018-02-26 08:17:11 -0800

    Rewrite `flrt` in dart

--- README.md
+++ README.md
@@ -4,29 +4,73 @@
 
 ## Status
 
-Brand new. Not ready for use.
+Brand new. Might eat your homework.
 
 ## Goals
 
 - To be sunsetted by the addition of `flutter packages add <package name>` and `flutter packages remove <package name>` commands
 - To wrap `flutter` and make dependency manipulation easier
 
-## Build `flrt`
+## Run `flrt` via `dart`
 
 ```sh
-$ cargo build
-```
-
-## Run `flrt` via `cargo`
-
-```sh
-$ cargo run -- <package name>
+$ dart src/main.dart <command> <packages>
 ```
 
 The above will work if there is a `pubspec.yaml` file in the current working directory.
 
-If that is not the case you will need to provide the path to a valid manifest.
+If that is not the case you will need to provide the path to a directory containing a valid manifest.
 
 ```sh
-$ cargo run -- <package name> --manifest-path /path/to/a/pubspec.yaml
+$ dart src/main.dart <command> --manifest-directory /path/to/a/directory <packages>
 ```
+
+## Example invocations
+
+Assuming you have a `./pubspec.yaml` file with the following contents.
+
+```yaml
+dependencies: 
+  args: "1.4.1"
+  pubspec: "0.0.15"
+```
+
+Run the following command.
+
+```sh
+$ dart src/main.dart add path_provider
+```
+
+Observe that your `./pubspec.yaml` file has been modified like so.
+
+```yaml
+dependencies: 
+  args: "1.4.1"
+  pubspec: "0.0.15"
+  path_provider: <unknown>
+```
+
+This demonstrates the `add` command. The inverse of that command is `remove`.
+
+Again given the initial manifest.
+
+```yaml
+dependencies: 
+  args: "1.4.1"
+  pubspec: "0.0.15"
+```
+
+```sh
+$ dart src/main.dart remove pubspec
+```
+
+Will modify the manifest like so.
+
+```yaml
+dependencies: 
+  args: "1.4.1"
+```
+
+## License
+
+See the [LICENSE](./LICENSE) file.
---
+++ src/main.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:pubspec/pubspec.dart';
+import 'package:args/command_runner.dart';
+
+Future main(List<String> arguments) async {
+  var _ = new CommandRunner('flrt', 'Allows addition/removal of dependencies to/from a `pubspec.yaml` file')
+    ..addCommand(new AddCommand())
+    ..addCommand(new RemoveCommand())
+    ..run(arguments);
+}
+
+class AddCommand extends Command {
+  @override
+  String get description => 'add dependencies to a given pubspec.yaml file';
+
+  @override
+  String get name => 'add';
+
+  AddCommand() {
+    argParser.addOption("manifest-directory", defaultsTo: ".");
+  }
+
+  Future<Null> run() async {
+    var manifest_dir = argResults['manifest-directory'];
+    var d = new Directory(manifest_dir);
+    var spec = await PubSpec.load(d);
+
+    var newDeps = spec.dependencies;
+
+    for (var package_name in argResults.rest) {
+      if (!spec.dependencies.containsKey(package_name)) {
+        // TODO: Query the registry for the latest version of the given package.
+        newDeps[package_name] = '<unknown>';
+      }
+    }
+
+    spec = spec.copy(dependencies: newDeps);
+    spec.save(d);
+  }
+}
+class RemoveCommand extends Command {
+  @override
+  String get description => 'remove dependencies from a given pubspec.yaml file';
+
+  @override
+  String get name => 'remove';
+
+  RemoveCommand() {
+    argParser.addOption("manifest-directory", defaultsTo: ".");
+  }
+
+  Future<Null> run() async {
+    var manifest_dir = argResults['manifest-directory'];
+    var d = new Directory(manifest_dir);
+    var spec = await PubSpec.load(d);
+
+    var newDeps = spec.dependencies;
+
+    for (var package_name in argResults.rest) {
+      if (spec.dependencies.containsKey(package_name)) {
+        newDeps.remove(package_name);
+      }
+    }
+
+    spec = spec.copy(dependencies: newDeps);
+    spec.save(d);
+  }
+}
